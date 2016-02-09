fbba984188e22da806a5941752ede8ec2c7e2d96
Darek Rossman <darekrossman@me.com>
2016-02-09 12:42:09 -0500

    setup

--- test/app.test.js
+++ test/app.test.js
@@ -4,11 +4,6 @@ import { findDOMNode } from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import App from '../src/client/components/App';
 
-describe('A React application', () => {
-
-  it('should present a welcome message', () => {
-    let c = TestUtils.renderIntoDocument(<App />);
-    expect(findDOMNode(c).innerHTML).toEqual('Welcome to your pretty new app!');
-  });
+describe('A GIF search app', () => {
 
 });

b9d89c3c9e2fb2c100991d1e33a319ddc5f82e74
Darek Rossman <darekrossman@me.com>
2016-02-10 12:45:12 -0500

    starter

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,30 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import appStyles from './App.scss';
-import giphy from '../utils/giphy-api';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gifs: []
-    };
-  }
-
-  componentDidMount() {
-    this.props.store.dispatch({
-      types: ['FETCH_GIFS', 'FETCH_GIFS_SUCCESS', 'FETCH_GIFS_FAIL'],
-      promise: () => giphy.search('kittens')
-    });
-  }
-
   render() {
     return (
       <div>
-        {this.state.gifs.map((gif, i) => {
-          return (
-            <img key={i} src={gif.images.fixed_height.url}/>
-          );
-        })}
+        Hello there!
       </div>
     );
   }
